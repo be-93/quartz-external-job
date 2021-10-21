804cd3ca415d376b7539e22ae6181601c237cdf1
최웅석 <cus4703@gmail.com>
2021-10-21 15:26:04 +0900

    refactor: core module entity 책임 삭제

--- core/src/main/java/com/core/repository/CoreTestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.core.repository;
-
-import com.core.entity.CoreTest;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CoreTestRepository extends JpaRepository<CoreTest, Long> {
-}
--- core/src/main/java/com/core/repository/custom/QueryDslCustomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.core.repository.custom;
-
-import com.core.entity.CoreTest;
-
-import java.util.List;
-
-public interface QueryDslCustomRepository {
-    List<CoreTest> findAllCoreTest();
-}
