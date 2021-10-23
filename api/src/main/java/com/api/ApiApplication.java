d72662bf98853fac583ecc7c708efa5c39a4b9fc
cus4703 <cus4703@gmail.com>
2021-10-23 21:43:53 +0900

    feat: componentScan 추가

--- api/src/main/java/com/api/ApiApplication.java
+++ api/src/main/java/com/api/ApiApplication.java
@@ -2,8 +2,10 @@ package com.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.external", "com.internal"})
 public class ApiApplication {
 
     public static void main(String[] args) {
