3ad86c8e3f6a5bf747a468c6c0fbc851e0c08217
최웅석 <cus4703@gmail.com>
2021-10-27 16:05:04 +0900

    refactor: scheduler module datasource 의존성 변경
    internal module datasource 의존하도록 변경

--- api/src/main/java/com/api/ApiApplication.java
+++ api/src/main/java/com/api/ApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.api","com.external", "com.internal"})
+@ComponentScan(basePackages = {"com.api", "com.external", "com.internal", "com.scheduler"})
 public class ApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
--- scheduler/src/main/java/com/scheduler/configuration/quartz/QuartzConfiguration.java
+++ scheduler/src/main/java/com/scheduler/configuration/quartz/QuartzConfiguration.java
@@ -1,7 +1,9 @@
 package com.scheduler.configuration.quartz;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.quartz.QuartzProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -13,20 +15,18 @@ import java.util.Properties;
 
 @Slf4j
 @Configuration
+@RequiredArgsConstructor
 public class QuartzConfiguration {
 
-	@Autowired
-	private DataSource dataSource;
-
-	@Autowired
-	private QuartzProperties quartzProperties;
-
 	/**
-	 * Quartz 관련 설정
-	 *
-	 * @param applicationContext the applicationContext
-	 * @return SchedulerFactoryBean
+	 * Quartz DataSource 설정
+	 * Default: internalDataSource 으로 설정
+	 * 스케줄을 다른 디비에서 설정할 경우 DataSource 생성 하여 주입이 필요함.
 	 */
+	@Qualifier("internalDataSource")
+	final private DataSource dataSource;
+	final private QuartzProperties quartzProperties;
+	
 	@Bean
 	public SchedulerFactoryBean schedulerFactoryBean(ApplicationContext applicationContext) {
 		SchedulerFactoryBean schedulerFactoryBean = new SchedulerFactoryBean();
